ab2be125daee44fcec6f523729c77c65b4b12b61
EchoXBR <372183365@qq.com>
2016-12-14 11:45:52 +0800

    test

--- app/src/main/java/com/speedata/uhf/MainActivity.java
+++ app/src/main/java/com/speedata/uhf/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     private IUHFService iuhfService;
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
