b9bcc8c8acb85782ceb8d7b3992554bc2ffb57e9
EchoXBR <372183365@qq.com>
2016-12-23 15:24:28 +0800

    delect libs

--- app/src/main/java/com/speedata/uhf/MainActivity.java
+++ app/src/main/java/com/speedata/uhf/MainActivity.java
@@ -1,15 +1,9 @@
 package com.speedata.uhf;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 
 import com.speedata.libuhf.IUHFService;
-import com.speedata.libuhf.UHFManager;
-import com.speedata.libuhf.XinLianQilian;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,16 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void testUHF() {
-        iuhfService = UHFManager.getUHFService();
-        iuhfService.OpenDev();
-        iuhfService.inventory_start(new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                ArrayList<XinLianQilian.Tag_Data> cx = (ArrayList<XinLianQilian.Tag_Data>) msg.obj;
-//                System.out.println("===handleMessage==");
-                super.handleMessage(msg);
-            }
-        });
+//        iuhfService = UHFManager.getUHFService();
+//        iuhfService.OpenDev();
+//        iuhfService.inventory_start(new Handler() {
+//            @Override
+//            public void handleMessage(Message msg) {
+//                ArrayList<XinLianQilian.Tag_Data> cx = (ArrayList<XinLianQilian.Tag_Data>) msg.obj;
+////                System.out.println("===handleMessage==");
+//                super.handleMessage(msg);
+//            }
+//        });
     }
 //    private IUHFService iuhfService;
 //
--- libid2/src/main/java/com/speedata/libid2/IDReadCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.speedata.libid2;
-
-/**
- * Created by brxu on 2016/12/19.
- */
-
-public interface IDReadCallBack {
-    public void callBack(IDInfor infor);
-}
